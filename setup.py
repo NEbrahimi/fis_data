d0c7d5d0629b68829468508fb430aa3ea16aed12
Naz Ebrahimi <z3541106@ad.unsw.edu.au>
2023-10-03 17:28:10 +1100

    add more info

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@ from setuptools import setup, find_packages
 
 setup(
     name='fis_data',
-    version='0.1.0',
+    version='0.1.1',
     packages=find_packages(),
     install_requires=[
         'pandas>=1.0.0',
+        'openpyxl',
     ],
     entry_points={
         'console_scripts': [
@@ -16,7 +17,9 @@ setup(
     author='Naz Ebrahimi',
     description='A package for rearranging FIS data',
     long_description=open('README.md').read(),
+    long_description_content_type='text/markdown',
     url='https://github.com/NEbrahimi/fis_data',
+    license='MIT',
     classifiers=[
         'License :: OSI Approved :: MIT License',
         'Programming Language :: Python :: 3.11',
